3e676bcc368f1d29fc048ac967addb820a7b2507
bottolo <samuele.castiglia.98@gmail.com>
2023-08-04 12:40:26 +0200

    refactor: Changed some prefabs, added models and changed scene

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    public float rotationSpeed = 20f;
+    public Vector3 rotationAxis = Vector3.up;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Rotate the object around the given axis by rotationSpeed degrees per second
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+    }
+}
